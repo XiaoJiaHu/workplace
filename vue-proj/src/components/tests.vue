cf7bb9ebfed7b7363515381093a82300e81170f0
XiaoJiaHu <1216989342@qq.com>
2020-08-14 22:15:49 +0800

    8.14

---
+++ vue-proj/src/components/tests.vue
@@ -0,0 +1,27 @@
+<template>
+    <h2 class = "yellow">testssss...{{mes}}</h2>
+</template>
+
+<script>
+export default {
+    name : 'testssss',
+    porps : {
+        mes : {
+            type : String,
+            default : 'testssss msg'
+        }
+    },
+    methods : {
+    },
+    data(){
+        return {}
+    },
+
+}
+</script>
+
+<style>
+.yellow{
+    color:yellow;
+}
+</style>
